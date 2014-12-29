ce003571b26cc9e3365837d7ef0c87e954c05e11
Amos Treiber <amos.treiber@gmail.com>
2014-12-29 16:04:48 +0100

    added login req for admin.php

--- www/admin.php
+++ www/admin.php
@@ -1,39 +1,68 @@
+<?php
+#set_include_path('~/Documents/robotikfp/matomat/www');
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+ 
+sec_session_start();
+?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
-		<title>Admin-Schnittstelle</title>
-	<body>
-	<h2>Transaction table</h2>
-	<table id="Header">
-		<tr>
-			<th>Username</th>
-			<th>UserID</th>
-			<th>Time</th>
-		</tr>
-<?php
-$servername	= "localhost";
-$username 	= "matomat";
-$password	= "matomat94";
-$dbname 	= "matomat";
-$conn = new mysqli($servername, $username, $password, $dbname);
-//catch failed connections
-if($conn->connect_error){
-	die("Couldn't connect to db: " . $conn->connect_error);}
-$sql = "SELECT * from TRANSACTIONS";
-$result = $conn->query($sql);
-if ($result->num_rows > 0){
-	//output data in a table
-	while($row = $result->fetch_row()){
-		echo "<tr> ";
-		for($x=0; $x < count($row); $x++){
-			echo "<td> " . $row[$x] . "</td>";	
-			}
-		echo "</tr>";
-	}
-}
-$conn->close();
-?>	
-	</table>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Admin-Schnittstelle</title>
+        <link rel="stylesheet" href="stylesheet.css" />
+        <script type="text/JavaScript" src="js/sha512.js"></script> 
+        <script type="text/JavaScript" src="js/forms.js"></script> 
+    </head>
+    <body>
+        <?php if (login_check($mysqli) == true) : ?>
+            <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>
+            <p><a href="includes/logout.php">logout</a></p>
+
+            <h2>Transaction table</h2>
+    <table id="Header">
+            <tr>
+                <th>Username</th>
+                <th>UserID</th>
+                <th>Time</th>
+            </tr>
+    <?php
+    $servername = "localhost";
+    $username   = "php";
+    $password   = "matomat94";
+    $dbname     = "matomat";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //catch failed connections
+    if($conn->connect_error){
+        die("Couldn't connect to db: " . $conn->connect_error);}
+    $sql = "SELECT * from TRANSACTIONS";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0){
+        //output data in a table
+        while($row = $result->fetch_row()){
+            echo "<tr> ";
+            for($x=0; $x < count($row); $x++){
+                echo "<td> " . $row[$x] . "</td>";  
+                }
+            echo "</tr>";
+        }
+    }
+    $conn->close();
+    ?>  
+    </table>
+        <?php else : ?>
+            <p>
+                <span class="error">You are not authorized to access this page.</span> Please login:
+            </p>
+            <form action="includes/process_login.php" method="post" name="login_form">                      
+            Email: <input type="text" name="email" />
+            Password: <input type="password" 
+                             name="password" 
+                             id="password"/>
+            <input type="button" 
+                   value="Login" 
+                   onclick="formhash(this.form, this.form.password);" /> 
+            </form>
+        <?php endif; ?>
+    </body>
 </html>
--- www/protected_page.php
+++ www/protected_page.php
@@ -1,5 +1,5 @@
 <?php
-set_include_path('~/Documents/robotikfp/matomat/www');
+#set_include_path('~/Documents/robotikfp/matomat/www');
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
  
@@ -9,23 +9,60 @@ sec_session_start();
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Secure Login: Protected Page</title>
-        <link rel="stylesheet" href="styles/main.css" />
+        <title>Admin-Schnittstelle</title>
+        <link rel="stylesheet" href="stylesheet.css" />
+        <script type="text/JavaScript" src="js/sha512.js"></script> 
+        <script type="text/JavaScript" src="js/forms.js"></script> 
     </head>
     <body>
         <?php if (login_check($mysqli) == true) : ?>
             <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>
-            <p>
-                This is an example protected page.  To access this page, users
-                must be logged in.  At some stage, we'll also check the role of
-                the user, so pages will be able to determine the type of user
-                authorised to access the page.
-            </p>
-            <p>Return to <a href="index.php">login page</a></p>
+            <p><a href="includes/logout.php">logout</a></p>
+
+            <h2>Transaction table</h2>
+    <table id="Header">
+            <tr>
+                <th>Username</th>
+                <th>UserID</th>
+                <th>Time</th>
+            </tr>
+    <?php
+    $servername = "localhost";
+    $username   = "php";
+    $password   = "matomat94";
+    $dbname     = "matomat";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //catch failed connections
+    if($conn->connect_error){
+        die("Couldn't connect to db: " . $conn->connect_error);}
+    $sql = "SELECT * from TRANSACTIONS";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0){
+        //output data in a table
+        while($row = $result->fetch_row()){
+            echo "<tr> ";
+            for($x=0; $x < count($row); $x++){
+                echo "<td> " . $row[$x] . "</td>";  
+                }
+            echo "</tr>";
+        }
+    }
+    $conn->close();
+    ?>  
+    </table>
         <?php else : ?>
             <p>
-                <span class="error">You are not authorized to access this page.</span> Please <a href="index.php">login</a>.
+                <span class="error">You are not authorized to access this page.</span> Please login:
             </p>
+            <form action="includes/process_login.php" method="post" name="login_form">                      
+            Email: <input type="text" name="email" />
+            Password: <input type="password" 
+                             name="password" 
+                             id="password"/>
+            <input type="button" 
+                   value="Login" 
+                   onclick="formhash(this.form, this.form.password);" /> 
+            </form>
         <?php endif; ?>
     </body>
 </html>
